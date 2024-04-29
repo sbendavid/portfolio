f6358afcf69b607b20c193b25784c4f0e887e838
sbendavid <samuelbendavid01@gmail.com>
2024-04-29 01:47:59 +0100

    Created the contact system

--- backend/main/views.py
+++ backend/main/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail
+
+from .forms import ContactForm
 
 
 def index(request):
-    return render(request, "backend/index.html", {
-    })
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            subject = 'New Contact Message'
+            message = f'Name: {form.cleaned_data['name']}\nEmail: {form.cleaned_data['email']}\nMessage: {form.cleaned_data['message']}'
+            sender = form.cleaned_data['email']
+            recipient = ['samuelbendavid01@gmail.com']
+
+            send_mail(subject, message, sender, recipient, fail_silently=False)
+            return redirect('index')
+    else:
+        form = ContactForm()
+    return render(request, 'backend/index.html', {'form': form}
+    )
